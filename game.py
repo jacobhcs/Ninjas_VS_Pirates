862a6f54f8a729b4c2903a2d32c212a7ea9a20c5
jacobhcs <jaharr1999@gmail.com>
2024-01-10 19:25:43 -0500

    stuff

--- game.py
+++ game.py
@@ -5,7 +5,7 @@ from classes.pirate import Pirate
 michelangelo = Ninja("Michelanglo")
 jack_sparrow = Pirate("Jack Sparrow")
 
-# FIGHT BEGINS HERE
+# FIGHT BEGINS HERE. LOOPS THROUGH CODE TILL UNTIL ONE PLAYERS HEALTH IS 0 OR BELOW
 while michelangelo.health > 0 and jack_sparrow.health > 0:
     jack_sparrow.attack(michelangelo)
     michelangelo.attack(jack_sparrow)
